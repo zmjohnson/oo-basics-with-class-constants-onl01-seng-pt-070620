f1966d78bd604047e4b9518d2add692628e2f5bb
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-24 01:43:52 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,24 +1,20 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
-require 'pry'
-
-class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
 
+  GENRES = []
+
   def initialize(title)
     @title = title
+ @@ -10,4 +12,9 @@ def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
-
-  GENRES = []
 
   def genre=(genre)
     @genre = genre
-    GENRES << genre unless GENRES.any? { |g| g == genre }
-  end
+    GENRES << genre
+  end 
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-end
+end 
